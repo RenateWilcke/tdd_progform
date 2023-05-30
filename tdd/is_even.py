d5cc150decdd6f183da636425362e5cb16a84688
RenateWilcke <renate.wilcke@smhi.se>
2023-05-30 13:36:51 +0200

    new function is_even with documentation

---
+++ tdd/is_even.py
@@ -0,0 +1,8 @@
+def is_even(x):
+    """
+        Determine if a number is even.
+
+        Return True if x is even.
+        Return False if x is odd.
+        Return TypeError if x is not a number.
+    """
