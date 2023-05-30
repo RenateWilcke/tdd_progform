2a1a136ba693e0197778b3161dfba4f7c82f172d
RenateWilcke <renate.wilcke@smhi.se>
2023-05-30 14:12:56 +0200

    is_odd works also for '2'

--- tdd/is_odd.py
+++ tdd/is_odd.py
@@ -1,3 +1,18 @@
+def is_even(x):
+    """
+        Determine if a number is even.
+
+        Return True if x is even.
+        Return False if x is odd.
+        Return TypeError if x is not a number.
+    """
+    if (x % 2) == 0:
+        return True
+    else:
+        return f"{x} is odd"
+
+
+
 def is_odd(x):
     """
         Determine if a number is odd.
@@ -6,7 +21,11 @@ def is_odd(x):
         Return False if x is even.
         Return TypeError if x is not a number.
     """
-    return(True)
+    if not is_even(x):
+        return True
+    else:
+        return f"{x} is even"
     
 assert is_odd.__doc__ is not None
 assert is_odd(1)
+assert is_odd(2)
