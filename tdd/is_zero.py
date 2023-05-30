4df23026532f80827dd011211a0f9e9bb9817d5b
RenateWilcke <renate.wilcke@smhi.se>
2023-05-30 13:20:11 +0200

    add test is_zero.py

---
+++ tdd/is_zero.py
@@ -0,0 +1,10 @@
+def is_zero(x):
+    """
+    Determine if a number is zero.
+
+    Rerurn True if x is an integer that is zero.
+    Return False if x is an integer that is not zero.
+    Raise TypeError if not a number.
+    """
+
+assert is_zero.__doc__ is not None
