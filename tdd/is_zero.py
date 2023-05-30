b705a7966a3ea98621b9995680ceb0503deaafab
RenateWilcke <renate.wilcke@smhi.se>
2023-05-30 13:28:23 +0200

    is_zero gives a result

--- tdd/is_zero.py
+++ tdd/is_zero.py
@@ -6,5 +6,17 @@ def is_zero(x):
     Return False if x is an integer that is not zero.
     Raise TypeError if not a number.
     """
+    if not isinstance(x, int):
+        raise TypeError("'x' must be an integer")
+    return True
 
 assert is_zero.__doc__ is not None
+
+has_raised = False
+try:
+    is_zero('asdf')
+except:
+    has_raised = True
+assert has_raised
+
+assert is_zero(0)
